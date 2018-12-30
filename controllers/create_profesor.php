bfeaf462823c558fede841077ce060779290c3bd
GianBros <GianBros@192.168.1.92>
2018-12-30 10:31:45 -0600

    Se elimino el campo usuario y ahora es el correo el que funjirá como usuario

--- controllers/create_profesor.php
+++ controllers/create_profesor.php
@@ -1,47 +1,56 @@
 <?php
 
-    include('../config/conexion.php');
-    include('../config/variables.php');
-    
-    $idPaq = $_POST['inputPaq'];
-    $name = $_POST['inputName'];
-    $user = $_POST['inputUser'];
-    $pass = $_POST['inputPass'];
-    $cel = (isset($_POST['inputCel'])) ? $_POST['inputCel'] : "";
-    $mail = $_POST['inputMail'];
+include('../config/conexion.php');
+include('../config/variables.php');
 
-    $cadErr = '';
+$idPaq = $_POST['inputPaq'];
+$name = $_POST['inputName'];
+$user = $_POST['inputMail'];
+$pass = $_POST['inputPass'];
+$cel = (isset($_POST['inputCel'])) ? $_POST['inputCel'] : "";
+$mail = $_POST['inputMail'];
+
+$cadErr = '';
+$ban = false;
+
+//Obtenemos número de registros
+$sqlGetNumProfs = "SELECT id FROM $tProf ";
+$resGetNumProfs = $con->query($sqlGetNumProfs);
+$getNumProfs = $resGetNumProfs->num_rows;
+$clave = strtolower($user) . ($getNumProfs + 1);
+
+//Buscamos si ya existe el usuario
+$sqlSearchUser = "SELECT id FROM $tProf WHERE user = '$user' ";
+$resSearchUser = $con->query($sqlSearchUser);
+if ($resSearchUser->num_rows > 0) {
+    $cadErr = 'Error: El usuario ya existe.';
     $ban = false;
-    
-    //Obtenemos número de registros
-    $sqlGetNumProfs = "SELECT id FROM $tProf ";
-    $resGetNumProfs = $con->query($sqlGetNumProfs);
-    $getNumProfs = $resGetNumProfs->num_rows;
-    $clave = strtolower($user).($getNumProfs+1);
-    
+} else {//Si no existe
     $sqlInsertInfo = "INSERT INTO $tInfo (celular, correo, foto_perfil, creado, actualizado) "
             . "VALUES ('$cel', '$mail', '$fotoPerfil', '$dateNow', '$dateNow') ";
-    if($con->query($sqlInsertInfo) === TRUE){
+    if ($con->query($sqlInsertInfo) === TRUE) {
         $idInfo = $con->insert_id;
         $sqlInsertUser = "INSERT INTO $tProf "
-            ."(nombre, user, pass, clave, informacion_id, creado, actualizado, activo, paquete_id) "
-            . "VALUES ('$name', '$user', '$pass', '$clave', '$idInfo', '$dateNow', '$dateNow', '1', '$idPaq') ";
-        if($con->query($sqlInsertUser) === TRUE){
+                . "(nombre, user, pass, clave, informacion_id, creado, actualizado, activo, paquete_id) "
+                . "VALUES ('$name', '$user', '$pass', '$clave', '$idInfo', '$dateNow', '$dateNow', '1', '$idPaq') ";
+        if ($con->query($sqlInsertUser) === TRUE) {
             $ban = true;
-        }else{
+        } else {
             $ban = false;
-            $cadErr .= 'Error al crear nuevo profesor<br>'.$con->error;
+            $cadErr .= 'Error al crear nuevo profesor<br>' . $con->error;
         }
-    }else{
+    } else {
         $ban = false;
-        $cadErr .= 'Error al insertar información.<br>'.$con->error;
+        $cadErr .= 'Error al insertar información.<br>' . $con->error;
     }
-    
-    //$ban = true;
-    if($ban){
-        echo json_encode(array("error"=>0));
-    }else{
-        echo json_encode(array("error"=>1, "msgErr"=>$cadErr));
-    }
-    
+}
+
+
+//$ban = true;
+if ($ban) {
+    $cadErr = 'Éxito, tu usuario ha sido creado con éxito.';
+    echo json_encode(array("error" => 0, "msgErr" => $cadErr));
+} else {
+    echo json_encode(array("error" => 1, "msgErr" => $cadErr));
+}
 ?>
